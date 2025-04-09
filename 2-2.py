a80bb68d4272cf879401ec21bff62b14fc23f5da
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-09 17:40:08 +0530

    Loops

---
+++ 2-2.py
@@ -0,0 +1,11 @@
+name = "Alice"  # String
+age = 20        # Integer
+height = 5.6    # Float
+is_student = True  # Boolean
+
+
+# Loops
+print("\n--- Loops ---")
+print("Counting from 1 to 5:")
+for i in range(1, 6):
+    print(i)
