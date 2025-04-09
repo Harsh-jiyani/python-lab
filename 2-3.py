cd8f560908482945594cc5ca16638a09d0b96da2
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-09 17:40:35 +0530

    Lists

---
+++ 2-3.py
@@ -0,0 +1,12 @@
+name = "Alice"  # String
+age = 20        # Integer
+height = 5.6    # Float
+is_student = True  # Boolean
+
+
+#  Lists
+print("\n--- Lists ---")
+fruits = ["apple", "banana", "cherry"]
+print("Fruits:", fruits)
+print("First fruit:", fruits[0]) 
+print("Number of fruits:", len(fruits))
