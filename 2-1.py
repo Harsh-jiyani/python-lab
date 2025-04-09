9471a6c5ef182b6d707e76519a8f0328b5106196
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-09 17:39:37 +0530

    Functions

---
+++ 2-1.py
@@ -0,0 +1,11 @@
+name = "Alice"  # String
+age = 20        # Integer
+height = 5.6    # Float
+is_student = True  # Boolean
+
+#  Functions
+print("\n--- Functions ---")
+def greet(person):
+    return "Hello, " + person + "!"
+
+print(greet(name))
