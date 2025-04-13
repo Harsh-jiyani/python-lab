fa51e6f82034bc83b5a0d91cff78ad8bb25aecdc
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-13 14:42:33 +0530

    Age Calculator

---
+++ 6-7.py
@@ -0,0 +1,9 @@
+#Age Calculator
+
+from datetime import date
+
+birth_year = int(input("Enter your birth year: "))
+current_year = date.today().year
+age = current_year - birth_year
+
+print("You are", age, "years old.")
