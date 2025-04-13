28127e8f638cd33b6d90baba1c3f16cc92129e87
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-13 14:36:37 +0530

    #Count Vowels in a String

---
+++ 6-4.py
@@ -0,0 +1,10 @@
+#Count Vowels in a String
+text = input("Enter a sentence: ").lower()
+vowels = "aeiou"
+count = 0
+
+for char in text:
+    if char in vowels:
+        count += 1
+
+print("Number of vowels:", count)
