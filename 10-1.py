85846a2a6d623096f760419cc903be4127dccc15
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-05-08 10:56:03 +0530

    Demonstrate list

---
+++ 10-1.py
@@ -0,0 +1,23 @@
+# Demonstrate list operations in Python
+
+ 
+fruits = ['apple', 'banana', 'cherry', 'date']
+
+ 
+first_fruit = fruits[0]
+last_fruit = fruits[-1]
+
+ 
+some_fruits = fruits[1:3]
+
+ 
+fruits.append('elderberry')
+
+ 
+fruits.remove('banana')
+
+ 
+print("Original list after modifications:", fruits)
+print("First fruit:", first_fruit)
+print("Last fruit:", last_fruit)
+print("Sliced fruits:", some_fruits)
